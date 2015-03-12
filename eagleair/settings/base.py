6861064b6a345775becefd372e6f80c2902603d3
Kaung Htet Zaw <emoosx@gmail.com>
2015-03-13 07:42:30 +0800

    Setup bower

--- eagleair/settings/base.py
+++ eagleair/settings/base.py
@@ -2,7 +2,7 @@ import dj_database_url
 import os
 
 PROJECT_ROOT = os.path.abspath(
-    os.path.join(os.path.dirname(__file__),
+    os.path.join(os.path.dirname(__file__), '..')
 )
 
 SECRET_KEY = '5nn1-c3=77+kq6k7q9mzt8q51(2x)n$771mu+iueeq1(z7i034'
@@ -11,17 +11,6 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = ['*']
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-ROOT_URLCONF = 'eagleair.urls'
-WSGI_APPLICATION = 'eagleair.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config()
@@ -45,6 +34,23 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'djangobower.finders.BowerFinder',
 )
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+ROOT_URLCONF = 'eagleair.urls'
+WSGI_APPLICATION = 'eagleair.wsgi.application'
+
 BOWER_COMPONENTS_ROOT = os.path.join(PROJECT_ROOT, 'components')
 
 DJANGO_APPS = (
@@ -59,6 +65,7 @@ LOCAL_APPS = (
     'eagleair',
     'flight',
     'reservation',
+    'website',
 )
 THIRD_PARTY_APPS = (
     'materialize',
